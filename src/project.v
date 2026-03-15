76c1611293d39b3a8acafbaa63bfc74a4f8d245d
Luca Caballero Cusin <lcaballero@student.ethz.ch>
2026-03-15 18:32:37 +0100

    first commit

--- src/project.v
+++ src/project.v
@@ -5,6 +5,7 @@
 
 `default_nettype none
 
+/*
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -20,6 +21,95 @@ module tt_um_example (
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
+
+endmodule
+*/
+
+module tt_um_sat_add (
+    input  wire [7:0] a_ui_in,    // Dedicated inputs
+    input  wire [7:0] b_ui_in,
+    input wire [1:0] sel,
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // All output pins must be assigned. If not used, assign to 0.
+  //assign uo_out  = a_ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  logic [8:0]raw_sum;
+  logic [8:0]raw_sub;
+  logic [7:0]result;
+
+
+
+  assign uo_out=result;
+  
+
+  always_comb begin
+    result = 8'h00;
+    // Function code goes here
+    raw_sum=unsigned'({1'b0,a_ui_in}) + unsigned'({1'b0,b_ui_in});
+    raw_sub=unsigned'({1'b0,a_ui_in}) + unsigned'({1'b1,~b_ui_in} + 1'b1);
+    case(sel)
+      2'b00: begin//SAT_ADD
+        result = raw_sum[7:0];
+        case(raw_sum[7])
+          1'b1: begin
+            if (a_ui_in[7]==0 & b_ui_in[7]==0) //sum of two pos becomes neg
+              result=8'b0111_1111;
+          end
+          1'b0: begin
+            if (a_ui_in[7]==1 & b_ui_in[7]==1) //sum of two pos becomes neg
+              result=8'b1000_0000;
+          end
+          default : result=raw_sum[7:0];
+        endcase
+      end
+      2'b01: begin//SAT_SUB
+        result = raw_sub[7:0];
+        case(raw_sub[7])
+            1'b0: begin
+              if (a_ui_in[7]==1 & b_ui_in[7]==0) //sub of neg and pos becomes pos
+                result=8'b1000_0000;
+            end
+            1'b1: begin
+              if (a_ui_in[7]==0 & b_ui_in[7]==1) //sub of pos and neg becomes neg
+                result=8'b0111_1111;
+            end
+            default : result=raw_sub[7:0];
+          endcase
+      end
+      2'b10: begin//SAT_ADD_U
+      result = raw_sum[7:0];
+        case(raw_sum[8])
+            1'b1: begin //overflow
+                result[7:0]='1;
+            end
+            default : result=raw_sum[7:0];
+          endcase
+      end
+      2'b11: begin//SAT_SUB_U
+      result = raw_sub[7:0];
+      case(raw_sub[8])
+            1'b1: begin //overflow
+                result[7:0]='0;
+            end
+            default : result=raw_sub[7:0];
+          endcase
+        end
+        default: result = 8'h00;
+    endcase
+  end
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
--- test/tb.v
+++ test/tb.v
@@ -14,18 +14,23 @@ module tb ();
   end
 
   // Wire up the inputs and outputs:
+
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
+  reg [7:0] a_ui_in;  // Dedicated inputs
+  reg [7:0] b_ui_in;
+  reg [1:0] sel;
   reg [7:0] uio_in;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
+  tt_um_sat_add user_project(
+      .a_ui_in  (a_ui_in),    // Dedicated inputs
+      .b_ui_in  (b_ui_in),
+      .sel  (sel),
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
@@ -34,5 +39,156 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
+
+  initial begin
+
+    rst_n = 0;
+    sel = 2'b00;
+    a_ui_in = 8'b0111_1111;
+    b_ui_in = 8'b0000_0001;
+
+    $display("TEST 1.1: SAT ADD (SAT CASE)");
+
+    #1; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b0111_1111) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #10;
+    $display("TEST 1.2: SAT ADD (NO SAT CASE)");
+    
+    rst_n = 0;
+    sel = 2'b00;
+    a_ui_in = 8'b0000_0000;
+    b_ui_in = 8'b0000_0001;
+
+    #11; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b0000_0001) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #20;
+    $display("TEST 2.1: SAT SUB (SAT CASE)");
+
+    rst_n = 0;
+    sel = 2'b01;
+    a_ui_in = 8'b1000_0000;
+    b_ui_in = 8'b0000_0001;
+
+    #21; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b1000_0000) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #30;
+    $display("TEST 2.2: SAT SUB (NOT SAT CASE)");
+
+    rst_n = 0;
+    sel = 2'b01;
+    a_ui_in = 8'b0000_0011;
+    b_ui_in = 8'b0000_0001;
+
+    #31; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b0000_0010) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #40;
+    $display("TEST 3.1: SAT ADD U (SAT CASE)");
+
+    rst_n = 0;
+    sel = 2'b10;
+    a_ui_in = 8'b1111_1111;
+    b_ui_in = 8'b0000_0001;
+
+    #41; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b1111_1111) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #50;
+    $display("TEST 3.2: SAT ADD U (NO SAT CASE)");
+    
+    rst_n = 0;
+    sel = 2'b10;
+    a_ui_in = 8'b0000_0000;
+    b_ui_in = 8'b0000_0001;
+
+    #51; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b0000_0001) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #60;
+    $display("TEST 4.1: SAT SUB U (SAT CASE)");
+
+    rst_n = 0;
+    sel = 2'b11;
+    a_ui_in = 8'b0000_0000;
+    b_ui_in = 8'b0000_0001;
+
+    #61; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b0000_0000) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #70;
+    $display("TEST 4.2: SAT SUB U (NOT SAT CASE)");
+
+    rst_n = 0;
+    sel = 2'b11;
+    a_ui_in = 8'b0000_0011;
+    b_ui_in = 8'b0000_0001;
+
+    #71; 
+
+    $display("At time %t, Output is: %b (hex: %h)", $time, uo_out, uo_out);
+
+    if (uo_out == 8'b0000_0010) begin
+      $display("Result: GOOD RESULT !");
+    end else begin
+      $display("Result: BAD RESULT !");
+    end
+    
+    #80;
+    $finish;
+  end
+
+
 
 endmodule
